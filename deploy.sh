bc21a5ee538fd0aafdaa7598d69ca4d1a74a446e
Archer Godson <archergodson@gmail.com>
2021-07-29 14:38:38 +0300

    addin .oh-my-zsh submodule update to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 REPO="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/"
 HOME="$(eval echo ~$USER)/"
+
+cd $REPO
+git submodule init
+git submodule update
+cd -
+
 echo "Syncing $REPO in $HOME"
 rsync -av --progress $REPO $HOME --exclude .git --exclude deploy.sh
