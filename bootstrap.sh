0251c974b18f3e8ab1eb1ee977df2d6cb9d77b34
AG <archergodson@gmail.com>
2026-08-28 15:20:56 +0300

    Add package check to bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,8 +6,40 @@ MAIN_SCRIPT="deploy.sh"
 REPO_NAME="env-master"
 URL="https://github.com/ArcherGodson/env/archive/refs/heads/master.zip"
 
+REQUIRED_PKG=""
+if ! command -v unzip &> /dev/null; then
+    echo "[-] Утилита 'unzip' не найдена."
+    REQUIRED_PKG="$REQUIRED_PKG unzip"
+fi
+
+if ! command -v rsync &> /dev/null; then
+    echo "[-] Утилита 'rsync' не найдена."
+    REQUIRED_PKG="$REQUIRED_PKG rsync"
+fi
+
+if [ -n "$REQUIRED_PKG" ]; then
+    echo "[*] Пытаемся автоматически установить отсутствующие пакеты:$REQUIRED_PKG..."
+    
+    SUDO_CMD=""
+    if [ "$EUID" -ne 0 ] && command -v sudo &> /dev/null; then
+        SUDO_CMD="sudo"
+    fi
+
+    if command -v apt-get &> /dev/null; then
+        $SUDO_CMD apt-get update -qq
+        $SUDO_CMD apt-get install -y $REQUIRED_PKG
+    elif command -v dnf &> /dev/null; then
+        $SUDO_CMD dnf install -y $REQUIRED_PKG
+    elif command -v yum &> /dev/null; then
+        $SUDO_CMD yum install -y $REQUIRED_PKG
+    else
+        echo "[!] Package manager not found (apt/dnf/yum)."
+        echo "[!] Install packages manualy:$REQUIRED_PKG"
+        exit 1
+    fi
+fi
+
 TMP_DIR=$(mktemp -d)
-echo "$TMP_DIR created"
 cd "$TMP_DIR"
 
 curl -sL "$URL" -o repo.zip
